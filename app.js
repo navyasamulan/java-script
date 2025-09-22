4574178267363c0736022a771b83d308c42c3856
navyasamulan <navyasamulan@gmail.com>
2025-09-22 10:32:47 +0530

    update changes

---
+++ app.js
@@ -0,0 +1,16 @@
+//types of variables
+//var,let,const
+//var global- variables
+//let,const - local variable
+
+var a = 67;
+var a = 34;
+let b = 68;
+
+const c = 78;
+
+console.log('value b is $(b)');
+console.log('value c is $(b)');
+
+console.log(a);
+
